5aeb496a58b843bff65be50068f7713c1326116e
Daniel Jalleh <djalleh@u80z06s03.42kl.edu.my>
2023-11-21 23:05:22 +0800

    done stuff

---
+++ libft/ft_memmove.c
@@ -0,0 +1,67 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_memmove.c                                       :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: djalleh <djalleh@student.42.fr>            +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2023/11/09 17:57:19 by djalleh           #+#    #+#             */
+/*   Updated: 2023/11/09 22:24:11 by djalleh          ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "libft.h"
+
+void	*ft_memmove(void *dst, const void *src, size_t n)
+{
+	unsigned char	*memd;
+	unsigned char	*mems;
+	size_t			i;
+
+	i = 0;
+	memd = (unsigned char *)dst;
+	mems = (unsigned char *)src;
+	if (dst > src)
+	{
+		while (n > 0)
+		{	
+			n--;
+			memd[n] = mems[n];
+		}
+	}
+	else
+	{
+		while (i < n)
+		{
+			memd[i] = mems[i];
+			i++;
+		}
+	}
+	return (dst);
+}
+
+/*#include <string.h>
+#include <stdio.h>
+#include <stdlib.h>
+int main()
+{
+	//char *str = NULL;
+	//char *str2 = NULL;
+	//char *str = "Balls are heavy";
+	//char str2[6] = "Fined";
+
+	//memcpy(str, str2 ,5);
+	//ft_memcpy(str, str2 ,5);
+	//memmove(str + 2, str, 6);
+	
+	char str[] = "Hello, world!";
+    //ft_memmove(str + 7, str, 7); 
+	memmove(str + 6, str, 7); 
+    printf("%s\n", str); // Output: "world!Hello"
+
+    return 0;
+
+	//printf("%s\n", str);
+	//printf("%s\n", str2);
+	return (0);
+}*/
---
+++ libft/libft.h
@@ -0,0 +1,32 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   libft.h                                            :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: djalleh <djalleh@student.42.fr>            +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2023/11/01 18:50:34 by djalleh           #+#    #+#             */
+/*   Updated: 2023/11/21 20:55:14 by djalleh          ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#ifndef LIBFT_H
+# define LIBFT_H
+
+# include <stdlib.h>
+# include <unistd.h>
+
+int		ft_isalpha(int c);
+int		ft_isdigit(int c);
+int		ft_isalnum(int c);
+int		ft_isascii(int c);
+int		ft_isprint(int c);
+int		ft_strlen(char *str);
+void	*ft_memset(void *s, int c, size_t n);
+void	ft_bzero(void *s, size_t n);
+void	*ft_memcpy(void *restrict dst, const void *restrict src, size_t n);
+void	*ft_memmove(void *dst, const void *src, size_t n);
+size_t	ft_strlcpy(char *dst, char *src, size_t n);
+size_t	ft_strlcat(char *dst, char *src, size_t n);
+
+#endif
